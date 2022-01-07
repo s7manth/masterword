a29a148223e226a59f4cc2656e1de34249fa6f65
Sourabh Jaiswal <sourabhrj31@gmail.com>
2022-01-07 12:32:32 +0800

    integrate list and submit with ap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { useState } from 'react';
+
 import './App.css';
 import List from './components/List/List';
+import Submit from './components/Submit/Submit';
 
 function App() {
+  const [guessWordList, setGuessWordList] = useState([]);
+  const [guessResultList, setGuessResultList] = useState([]);
   return (
     <div className="App">
-      <List/>
+      <List guessWordList={guessWordList} guessResultList={guessResultList}/>
+      <Submit setGuessResultList={setGuessWordList} setGuessResultList={setGuessResultList}/>
     </div>
   );
 }
