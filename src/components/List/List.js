b962e3346cbb8692a2566402a9ff170a1026f976
Sourabh Jaiswal <sourabhrj31@gmail.com>
2022-01-07 12:11:55 +0800

    set up list and rows

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,6 +1,23 @@
+import { useState, useEffect} from "react";
 
-const List = () => {
-    return <div></div>
+import "./List.css";
+import Row from "./Row/Row";
+
+
+const List = (props) => {
+
+    const [wordListDisplay, setWordListDisplay] = useState([])
+
+
+    useEffect(() => {
+        setWordListDisplay(props.list);
+    }, [props])
+
+    return <div className={"listContainer"}>
+        <div>{[1,2,3].map((row) => {
+            return <Row word={"Hello"} result={3}/>
+        })}</div>
+    </div>
 }
 
 export default List;
--- src/components/List/Row/Row.js
+++ src/components/List/Row/Row.js
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
 
-const Row = () => {
-    return <div></div>
+import "./Row.css";
+
+const Row = (props) => {
+    const [word, setWord] = useState("")
+    const [result, setResult] = useState("");
+
+    useEffect(() => {
+        setWord(props.word);
+        setResult(props.result);
+    }, [props])
+
+
+    return (
+        <div className="rowContainer">
+            <div>{word}</div>
+            <div>{result}</div>
+        </div>
+    )
 }
 
 export default Row;
