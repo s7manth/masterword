86a434f148e56b2c403eca7e5cdaa2f6013ac342
Shivang211 <shivanggupta2110@gmail.com>
2022-01-07 13:56:20 +0800

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
+import List from './components/List/List';
+import BasicModal from './components/Modal/Modal';
 import NavBar from './components/NavBar/NavBar';
+import Submit from './components/Submit/Submit';
 
 function App() {
+  const [guessWordList, setGuessWordList] = useState([]);
+  const [guessResultList, setGuessResultList] = useState([]);
+
+  const [instructionsOpen, setInstructionsOpen] = useState(false);
+  const handleOpen = () => setInstructionsOpen(true);
+  const handleClose = () => setInstructionsOpen(false);
+
   return (
     <div className="App">
       <NavBar/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BasicModal />
+      <List guessWordList={guessWordList} guessResultList={guessResultList}/>
+      <Submit setGuessResultList={setGuessWordList} setGuessResultList={setGuessResultList}/>
     </div>
   );
 }
