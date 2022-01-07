41ed6a23b557fd9c248b4f8bf467bd581711b99f
Sourabh Jaiswal <sourabhrj31@gmail.com>
2022-01-07 12:56:01 +0800

    abstract columns from list

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,23 +4,36 @@ import "./List.css";
 import Row from "./Row/Row";
 
 
-const List = (props) => {
-
+const List = ({guessWordList, guessResultList}) => {
     const [wordListColumn1, setWordListColumn1] = useState([])
     const [wordListColumn2, setWordListColumn2] = useState([])
 
+    const [resultListColumn1, setResultListColumn1] = useState([])
+    const [resultListColumn2, setResultListColumn2] = useState([])
+
+    const initializeColumns = () => {
+        const listLength = guessWordList.length;
+        if(listLength<=10) {
+            setWordListColumn1(guessWordList)
+            setResultListColumn1(guessResultList)
+        } else {
+            setWordListColumn1(guessWordList.slice(0,10));
+            setResultListColumn1(guessResultList.slice(0,10));
+            setWordListColumn2(guessWordList.slice(10, listLength));
+            setResultListColumn2(guessResultList.slice(10, listLength));
+        }
+    }
 
     useEffect(() => {
-        setWordListColumn1(props.list1);
-        setWordListColumn2(props.list2);
-    }, [props])
+        initializeColumns();
+    }, [guessWordList, guessResultList])
 
     return <div className={"listContainer"}>
-        <div className={"listColumn"}>{[1,2,3,4,5,6,7,8,9,10].map((row) => {
-            return <Row word={""} result={3}/>
+        <div className={"listColumn"}>{[1,2,3,4,5,6,7,8,9,10].map((index) => {
+            return <Row word={wordListColumn1[index]} result={resultListColumn1[index]}/>
         })}</div>
-         <div className={"listColumn"}>{[1,2,3,4,5,6,7,8,9,10].map((row) => {
-            return <Row word={""} result={3}/>
+         <div className={"listColumn"}>{[1,2,3,4,5,6,7,8,9,10].map((index) => {
+            return <Row word={wordListColumn2[index]} result={resultListColumn2[index]}/>
         })}</div>
     </div>
 }
